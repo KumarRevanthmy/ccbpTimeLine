eb10ca1d182037fa286b076f70dbf2cf41e9d548
revanth <revanth kumar>
2023-03-16 18:36:22 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css'
+
+// Replace your code here
+const App = () => <h1>Hello World</h1>
+
+export default App
+import {Route, Switch} from 'react-router-dom'
+
+import CourseItem from './components/CourseItem'
+
+import CourseItemDetails from './components/CourseItemDetails'
+
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+// Replace your code here
+const App = () => (
+  <div>
+    <Switch>
+      <Route exact path="/" component={CourseItem} />
+      <Route exact path="/courses/:id" component={CourseItemDetails} />
+      <Route component={NotFound} />
+    </Switch>
+  </div>
+)
+export default App
